b122153b696d91a093b0f3de5b57edaf4ead80fb
Codered1729 <203860248+Codered1729@users.noreply.github.com>
2026-08-31 23:46:31 +0530

    nothing

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,17 @@
 import express,{Request, Response} from "express";
 import pool from "./config/db"
 import cors from "cors"
+import dotenv from "dotenv"
+import path from "path"
+
+
+dotenv.config({path: path.resolve(__dirname , "../../.env")})
 
 const app = express();
-
+const PORT = process.env.PORT || 5000;
+const CLIENT_URL = process.env.CLIENT_URL || "http://localhost:5317" 
 app.use(cors({
-  origin : "http://localhost:5317",
+  origin : CLIENT_URL,
   credentials : true
 }))
 
@@ -15,10 +21,17 @@ app.get("/",(req: Request,res:Response)=>{
   res.send("hello");
 });
 
-pool.connect()
-  .then(() => console.log("✅ PostgreSQL Connected"))
-  .catch((err) => console.error(err));
-
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
-});
+const startServer = async ()=>{
+  try{
+    const client = await pool.connect()
+    console.log("PostgreSQL Connected")
+    client.release()
+    app.listen(PORT, ()=>{
+      console.log('Server running on port : ${PORT}')
+    })
+  } catch(err){
+    console.log("database connection error:", err)
+    process.exit(1);
+  }
+}
+startServer()
